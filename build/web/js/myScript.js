bb5de39a0aa602c6c2b7404d27771849379a2ec7
Andrés Gutiérrez <andres.gutierrez.arcia@gmail.com>
2018-03-24 11:49:57 -0600

    Home v1 usando Materialize
    Terminada la v1 del home

---
+++ build/web/js/myScript.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+
+$('.carousel.carousel-slider').carousel({
+fullWidth: true,
+indicators: true
+});
+autoplay() //para hacer que el carrusel se mueva solo
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
